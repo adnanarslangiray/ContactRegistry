c4a213b40c9da9a3950874afa4c7405d8bdf2e51
adnan <adnan@ADNAN-PC>
2023-10-24 17:55:26 +0300

    Application Layer : added ValidationBehaviour

---
+++ Contact/Core/ContantRegistry.Application/PipelineBehaviours/ValidationBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace ContantRegistry.Application.PipelineBehaviours;
+
+public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var context = new ValidationContext<TRequest>(request);
+        var failures = _validators.Select(x => x.Validate(context))
+                                    .SelectMany(x => x.Errors)
+                                    .Where(x => x != null)
+                                    .ToList();
+
+        if (failures.Count > 0)
+        {
+            throw new ValidationException(failures);
+        }
+        return next();
+    }
+}
