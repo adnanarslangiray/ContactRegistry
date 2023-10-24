c751307e84dd68fc1ae71063a2fb94eececa0a14
adnan <adnan@ADNAN-PC>
2023-10-25 01:01:59 +0300

    Added Rabbit MQ Event bus

---
+++ EventBusArea/RabbitMQEventBus/DefaultRabbitMQPersistentConnection.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+
+namespace RabbitMQEventBus;
+
+public class DefaultRabbitMQPersistentConnection : IRabbitMQPersistentConnection
+{
+    public bool IsConnected => throw new NotImplementedException();
+
+    public IModel CreateModel()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryConnect()
+    {
+        throw new NotImplementedException();
+    }
+}
